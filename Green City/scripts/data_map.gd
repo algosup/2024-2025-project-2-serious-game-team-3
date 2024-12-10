2899e03b6dbc1e7f58995baefa03fd3da98e5c02
Manech <146005062+Manech-Laguens@users.noreply.github.com>
2024-12-10 16:36:11 +0100

    start at 60%

--- Green City/scripts/data_map.gd
+++ Green City/scripts/data_map.gd
@@ -2,5 +2,5 @@ extends Resource
 class_name DataMap
 
 @export var cash:int = 10000
-@export var pollution:float = 0
+@export var pollution:float = 60000
 @export var structures:Array[DataStructure]
